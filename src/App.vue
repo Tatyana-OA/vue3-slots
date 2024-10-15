540dbb6d35c2866d094610454f9ca1b8802cd742
TAsenova <tasenova@mentormate.com>
2024-10-15 14:52:37 +0300

    named slots, using v-slot and templates to assign content to slots

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import BaseButton from "./components/BaseButton.vue";
+import BlogLayout from "./components/BlogLayout.vue";
 import IconCommunity from "./components/icons/IconCommunity.vue";
 import IconEcosystem from "./components/icons/IconEcosystem.vue";
 </script>
@@ -15,6 +16,20 @@ import IconEcosystem from "./components/icons/IconEcosystem.vue";
         <IconCommunity />
       </div>
     </BaseButton>
+    <BlogLayout>
+      <template v-slot:header>
+        <h1>My Page</h1>
+      </template>
+      <template v-slot:main>
+        <p>My page content</p>
+      </template>
+      <template v-slot:sidebar>
+        <ul>
+          <li>Bullet 1</li>
+          <li>Bullet 2</li>
+        </ul>
+      </template>
+    </BlogLayout>
   </main>
 </template>
 
---
+++ src/components/BlogLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <slot name="header"></slot>
+  </header>
+  <main>
+    <slot name="main"> </slot>
+  </main>
+  <aside>
+    <slot name="sidebar"> </slot>
+  </aside>
+</template>
+
+<script>
+export default {};
+</script>
