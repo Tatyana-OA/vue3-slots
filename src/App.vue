5a7eac369172fefa8acae4f286f965a0e0323500
TAsenova <tasenova@mentormate.com>
2024-10-15 14:32:56 +0300

    Simple slot example. Slots instead of props to provide better flexibility

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import BaseButton from "./components/BaseButton.vue";
+import IconCommunity from "./components/icons/IconCommunity.vue";
+import IconEcosystem from "./components/icons/IconEcosystem.vue";
+</script>
+
+<template>
+  <header></header>
+
+  <main>
+    <BaseButton>
+      <div class="flex-btn">
+        <IconEcosystem />
+        <h3>Button Text</h3>
+        <IconCommunity />
+      </div>
+    </BaseButton>
+  </main>
+</template>
+
+<style>
+.flex-btn {
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+  align-items: center;
+}
+</style>
